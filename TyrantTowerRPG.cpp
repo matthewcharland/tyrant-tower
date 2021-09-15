e3688be843431adc8c3b532d6486726db6fb1b75
fluffyman2271 <90650681+fluffyman2271@users.noreply.github.com>
2021-09-15 13:31:55 -0500

    Add files via upload

---
+++ TyrantTowerRPG.cpp
@@ -0,0 +1,19 @@
+#include "prototype.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    cout << "Please don't adjust window size." << endl;
+    
+    cout << "########################################################################################################################" << endl;
+    cout << "#                                                                                                                      #" << endl;
+    cout << "#   HP:000/100   Gold: 1234    Strength: 10    Item: 00/10                                                             #" << endl;
+    cout << "#   Wepon: ----------------    Ajility:  10    Keys: 1 2 3                                                             #" << endl;
+    cout << "#                                                                                                                      #" << endl;
+    cout << "########################################################################################################################" << endl;
+    
+    OpeningScene();
+
+}
