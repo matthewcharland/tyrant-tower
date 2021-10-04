b040fb84f53aadca4d5861fac8b432dee7d250b3
joaniem <mcdonoughj@nicc.edu>
2021-10-04 15:21:45 -0500

    Add files via upload

---
+++ location arrays.cpp
@@ -0,0 +1,13 @@
+#include <string>
+
+using namespace std;
+
+
+
+string farmerRing[14] = { "Farm 1", "Farm 2", "Farm 3", "Farm 4", "Farm 5", "Farm 6", "Pond 1", "Pond 2", "Pond 3", "Windmill 1", "Windmill 2", "Windmill 3", "Dark Forest" , "Gate 1" };
+string craftsmanRing[10] = { "Leatherworker 1", "Leatherworker 2", "Cook 1", "Cook 2", "Blacksmith 1", "Blacksmith 2", "Weaver 1", "Weaver 2", "Gate 2", "Gate 1" };
+string eliteClassRing[8] = { "Rich Family 1", "Rich Family 2", "Rich Family 3", "Rich Family 4", "Rich Family 5", "Rich Family 6" , "Gate 3", "Gate 2" };
+string castle;
+
+
+
